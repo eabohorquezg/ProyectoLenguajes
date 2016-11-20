e8a6eb23eb9f02dc617873ebb798dc52510335e1
Edwin Alexander <eabohorquezg@unal.edu.co>
2016-11-20 02:24:36 -0500

    regla EXP00-J

--- ProyectoLenguajes/src/clases/MyVisitor.java
+++ ProyectoLenguajes/src/clases/MyVisitor.java
@@ -16,18 +16,30 @@ public class MyVisitor<T> extends Java8BaseVisitor<T>{
     private static ArrayList<String> methodNames = new ArrayList<String>();
     private static HashMap<Integer, String> rules = new HashMap<>();
     private static ArrayList<String> fileTypeVariables = new ArrayList<String>();    
+    private static ArrayList<String> stringTypeVariables = new ArrayList<String>();    
     private static PrintWriter reportFile = null;
     private static String failureInformation = "";
+    private static int line=0,column=0;
     
     static{
         rules.put(1, "MET09-J Classes that define an equals() method must also define a hashCode() method");
         rules.put(2, "MET01-J Never use assertions to validate method arguments");
         rules.put(3, "OBJ01-J Limit accessibility of fields");
         rules.put(4, "FIO02-J Detect and handle file-related errors");
+        rules.put(5, "EXP00-J Do not ignore values returned by methods");
         //add rules
     }
     
-    public static void generateFaultReport(){
+    static List<String> fileBooleanMethods = Arrays.asList(
+        "canExecute","canRead","canWrite","equals","exists","isAbsolute","isDirectory","isFile","isHidden",                
+        "createNewFile", "delete", "mkdir", "mkdirs", "renameTo", "setExecutable", "setLastModified",
+        "setReadOnly", "setReadable", "setWritable");          
+    
+    static List<String> stringMethods = Arrays.asList(
+        "chars","concat","endsWith","intern","replace","replaceAll","replaceFirst","split","startsWith", 
+        "subSequence","toLowerCase","toUpperCase","trim");
+    
+    public static void generateFaultReport(){        
         try {
             reportFile = new PrintWriter(new FileWriter("SecurityFailures.txt"));            
             reportFile.println("Security Failures: \n\n");                        
@@ -52,8 +64,8 @@ public class MyVisitor<T> extends Java8BaseVisitor<T>{
     
     public void endOfClass( Java8Parser.CompilationUnitContext ctx ){
         if( methodNames.contains("equals") && !methodNames.contains("hashCode") ){ 
-            int line = ctx.EOF().getSymbol().getLine();
-            int column = ctx.EOF().getSymbol().getCharPositionInLine()-1;
+            line = ctx.EOF().getSymbol().getLine();
+            column = ctx.EOF().getSymbol().getCharPositionInLine()-1;
             securityFailure(line,column,rules.get(1));             
         }    
     }
@@ -61,8 +73,8 @@ public class MyVisitor<T> extends Java8BaseVisitor<T>{
     @Override 
     public T visitAssertStatement(Java8Parser.AssertStatementContext ctx) {         
         if( ctx.getChild(0).getText().contains("assert") ){
-            int line = ctx.start.getLine();
-            int column = ctx.start.getCharPositionInLine();
+            line = ctx.start.getLine();
+            column = ctx.start.getCharPositionInLine();
             securityFailure(line, column, rules.get(2));            
         }              
         return visitChildren(ctx); 
@@ -78,28 +90,38 @@ public class MyVisitor<T> extends Java8BaseVisitor<T>{
     public T visitFieldDeclaration(Java8Parser.FieldDeclarationContext ctx) {         
         String type = ctx.unannType().getText();
         String id = ctx.variableDeclaratorList().variableDeclarator().get(0).variableDeclaratorId().Identifier().getText();        
-        if( type.equals("File") )
+        if( type.equals("File") ){
             fileTypeVariables.add(id);
+        }else if( type.equals("String") ){
+            stringTypeVariables.add(id);
+        }
         if( !ctx.getChild(0).getText().equals("private") ){
-            int line = ctx.start.getLine();
-            int column = ctx.start.getCharPositionInLine();
+            line = ctx.start.getLine();
+            column = ctx.start.getCharPositionInLine();
             securityFailure(line, column, rules.get(3));            
         }
         return visitChildren(ctx); 
     }    
     
     @Override 
-    public T visitMethodInvocation(Java8Parser.MethodInvocationContext ctx) { 
-        List<String> booleanMethods = Arrays.asList(
-        "createNewFile", "delete", "mkdir", "mkdirs", "renameTo", "setExecutable", "setLastModified",
-        "setReadOnly", "setReadable", "setWritable");                
+    public T visitMethodInvocation(Java8Parser.MethodInvocationContext ctx) {                 
         for (int i = 0; i < fileTypeVariables.size(); i++) {
             if( fileTypeVariables.get(i).equals(ctx.getChild(0).getText()) ){
-                if( booleanMethods.contains(ctx.getChild(2).getText()) ){
-                    fileTypeVariables.remove(i);
-                    int line = ctx.start.getLine();
-                    int column = ctx.start.getCharPositionInLine();
+                if( fileBooleanMethods.contains(ctx.getChild(2).getText()) ){
+                    //fileTypeVariables.remove(i);
+                    line = ctx.start.getLine();
+                    column = ctx.start.getCharPositionInLine();
                     securityFailure(line, column, rules.get(4));                    
+                }
+            }               
+        }        
+        for (int i = 0; i < stringTypeVariables.size(); i++) {
+            if( stringTypeVariables.get(i).equals(ctx.getChild(0).getText()) ){
+                if( stringMethods.contains(ctx.getChild(2).getText()) ){
+                    //stringTypeVariables.remove(i);
+                    line = ctx.start.getLine();
+                    column = ctx.start.getCharPositionInLine();
+                    securityFailure(line, column, rules.get(5));                    
                 }
             }               
         }        
@@ -110,12 +132,13 @@ public class MyVisitor<T> extends Java8BaseVisitor<T>{
     public T visitLocalVariableDeclaration(Java8Parser.LocalVariableDeclarationContext ctx) { 
         String type = ctx.unannType().getText();
         String id = ctx.variableDeclaratorList().variableDeclarator().get(0).variableDeclaratorId().Identifier().getText();        
-        if( type.equals("File") )
+        if( type.equals("File") ){
             fileTypeVariables.add(id);
+        }else if( type.equals("String") ){
+            stringTypeVariables.add(id);
+        }        
         return visitChildren(ctx); 
     }              
- 
-    
-    
+         
 }
 
